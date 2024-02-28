7a906ddad5633fc8d96b022be50b9b11122563b5
Banosss <100426223+Banosss@users.noreply.github.com>
2024-02-28 12:37:25 +0800

    Update script.js

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ loginLink.addEventListener('click',()=> {
     wrapper.classList.remove('active');
 });
 
+
 btnPopup.addEventListener('click',()=> {
     wrapper.classList.add('active-popup');
 });
